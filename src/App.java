cf8bdcdb3e6739c567ba8582b3312c4d2b5d0295
mjarecky <mattjarecky@yahoo.com>
2023-10-16 15:37:52 -0700

    add display method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        display("This is my display!");
+    }
+
+    public static void display(String message) {
+
+        int length = message.length();
+
+        String boarder = "+";
+
+        for (int i = 0; i < length + 2; i++) {
+            boarder += "-";
+        }
+
+        boarder += "+";
+
+        System.out.println(boarder);
+        System.out.println("| " + message + " |");
+        System.out.println(boarder);
     }
 }
